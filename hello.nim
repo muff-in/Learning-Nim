43156e439c0e2d974a14ca1cdd3ced8ceeb9be9f
muffin <navneetmuffin@pm.me>
2021-08-25 20:33:37 +0530

    first commit

---
+++ hello.nim
@@ -0,0 +1,9 @@
+echo "Enter first number: "
+let a = readLine(stdin)
+echo "Enter second number: "
+let b = readLine(stdin)
+
+if (a > b) :
+  echo  a, " is greater number."
+else:
+  echo  b, " is greater number."
---
+++ run.bat
@@ -0,0 +1,3 @@
+set projectName=hello
+nim c -r --verbosity:0 %projectName%.nim
+%projectName%.exe
