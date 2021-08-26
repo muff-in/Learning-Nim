6b6c4eb421604d5ea0d4393e6ae84b9a91797eee
muffin <navneetmuffin@pm.me>
2021-08-26 17:56:25 +0530

    master

--- helloworld.nim
+++ helloworld.nim
@@ -3,11 +3,12 @@ echo "Hello, World"
 # echo print out to the screen
 
 
-#Indentation-based syntax
+#[ Nim has mandatory indentation, identation is used to make the code more readable. If you don't know what identation is, you should google it.
+i.e. 4 spaces per indentation level, if you don't indent, it will not work 
+If you're using any modern code editor like VS code etc it will automatically indent for you.]#
 
 
 # Also this is how you do comments in nim
-
 
 # this is a single line comment 
 
